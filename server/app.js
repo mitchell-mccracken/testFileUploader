51f93efa6d9f18104c7c4bebb4c29b58c034a321
Mitch McCracken <mitchell@conturoprototyping.com>
2022-06-09 16:37:37 -0400

    Added functions for search and remove (remove  function doesn't seem to be working). Updated upload function to take in a file and send it to the appropirate nested part folder or create customer and part folder if needed. Need to add in error handling and remove hard coded values.

--- server/app.js
+++ server/app.js
@@ -2,7 +2,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const busboy = require('busboy');
-const { drive_v3, google } = require('googleapis');
+const { drive_v3, google, clouddebugger_v2 } = require('googleapis');
+const { GoogleAuth } = require('google-auth-library');
+const multer = require('multer');
 require('dotenv').config();
 
 
@@ -39,6 +41,8 @@ mongoose.connection.once('open', () => {
 app.get('/test', testRoute);
 
 app.post('/upload', upload);
+app.post('/search', search);
+app.post('/delete', remove)
 
 
 
@@ -55,48 +59,270 @@ app.all('*', (_req, res) => res.sendStatus(404));
 // FUNCTIONS //
 ///////////////
 
+async function remove(req, res){
+  //google auth setup
+  const credentials = {
+    private_key: process.env.SA_privateKey.replace(/\\n/g, '\n'),
+    client_email: process.env.SA_clientEmail
+  };
+  const auth = new GoogleAuth({ 
+    credentials, 
+    scopes: 'https://www.googleapis.com/auth/drive'
+  })
+
+  const service = google.drive({
+    version: 'v3',
+    auth,
+  });
+
+  const files = await service.files.list({
+    // q: `parents='0AG-3k0Os7-2KUk9PVA'`,
+    fields: 'files(id,name, trashed, parents)'
+  });
+  console.log(files.data.files)
+  console.log(files.data.files.length)
+  console.log(files.data.files[0]);
+  console.log(files.data.files[0].id);
+
+  const result = await service.files.delete({fileId: `'${files.data.files[0].id}'`});
+  console.log(result);
+
+  res.sendStatus(200)
+
+}
+
+
+async function search(_req, res){
+  console.log('---- in search function');
+  const folderId = '1C8G-Yy0d1Zzw9SzFdNuu6Qj3C1ANWnKc'    //to /testFolder
+  // const folderId = '1f7l0TJ_Pfm9YO2tfcyN3N1J6I7aaUawX'
+
+  //google auth setup
+  const credentials = {
+    private_key: process.env.SA_privateKey.replace(/\\n/g, '\n'),
+    client_email: process.env.SA_clientEmail
+  };
+  const auth = new GoogleAuth({ 
+    credentials, 
+    scopes: 'https://www.googleapis.com/auth/drive'
+  })
+
+  const service = google.drive({
+    version: 'v3',
+    auth,
+  })
+  // const customerId = "c_id_1"
+
+  // await service.files.delete({});
+  // console.log('files deleted')
+
+  const files = await service.files.list({
+    // q: 'trashed=false',
+    // q: 'hidden=false',
+    // q: 'name=\'timesheets_mongo_commands.txt\'',
+    // q: 'id=\'1QcB7mnKzIzdVz04LaOnXPrTQweNPhTwb\'',
+    // q: 'mimeType=\'text/plain\'',
+    // q: 'mimeType=\'application/vnd.google-apps.folder\'',
+    // q: 'name=\'testFolder\'',
+    // q: `name='${customerId}'`,
+    // q: "mimeType='application/vnd.google-apps.folder', 'name='testFolder''",
+    // q: 'parents=\'1C8G-Yy0d1Zzw9SzFdNuu6Qj3C1ANWnKc\'',   //sub folder
+    // q: 'parents=\'1f7l0TJ_Pfm9YO2tfcyN3N1J6I7aaUawX\'',
+    // q: `parents='0AG-3k0Os7-2KUk9PVA'`,
+    fields: 'files(id,name, trashed, parents)'
+
+  });
+  console.log(files.data.files)
+  console.log(files.data.files.length)
+
+  
+
+  res.sendStatus(200)
+
+
+}
+
 function testRoute(req, res){
   console.log('------ test route hit -------');
   res.status(200).send('Test route hit!');
 };
 
+//////////////////////////////
+// UPLOAD FILE FROM POSTMAN //
+//////////////////////////////
 async function upload(req, res){
   console.log('------ upload route --------');
-  const folderId = '1C8G-Yy0d1Zzw9SzFdNuu6Qj3C1ANWnKc'
-  console.log(req.body)
-  console.log(req.headers)
+  
+  // const folderId = '1C8G-Yy0d1Zzw9SzFdNuu6Qj3C1ANWnKc'    //to /testFolder
+  const folderId = '1f7l0TJ_Pfm9YO2tfcyN3N1J6I7aaUawX'
 
   const { headers } = req;
   const bb = busboy({ headers });
+  req.pipe(bb)
   bb._writableState.autoDestroy = false;
 
-  bb.on('close', () => res.sendStatus(201));
+  let data;
 
 
-  bb.on('file', (_name, fileStream, fileInfo) => {
-    console.log('------file')
+  bb.on('field', (fieldname, val) => {
+    data = JSON.parse(val);
+  })
+
+
+  
+
+
+  bb.on('file', async (_name, fileStream, fileInfo) => {
+    // console.log(_name, fileStream, fileInfo);
+    // console.log(data)
+    const { customerId, partNumber, partRev, partDescription } = data;
 
     const { filename, mimeType } = fileInfo;
-    const requestBody = {
-      mimeType,
-      name: filename,
-      parents: [ folderId ],
-    };
+    // const requestBody = {
+    //   mimeType,
+    //   name: filename,
+    //   parents: [ folderId ],
+    //   // customerId,
+    //   // partNumber,
+    //   // partRev,
+    //   // partDescription       //maybe this should be left out???
+    // };
 
     const media = {
       body: fileStream,
     };
 
-    // const drive = GetGoogleDrive()
-    const drive = google.drive({
-      version: 'v3',
-      auth: apiKey,
+    //google auth setup
+    const credentials = {
+      private_key: process.env.SA_privateKey.replace(/\\n/g, '\n'),
+      client_email: process.env.SA_clientEmail
+    };
+    const auth = new GoogleAuth({ 
+      credentials, 
+      scopes: 'https://www.googleapis.com/auth/drive'
     })
-    drive.files.create({
+
+    const service = google.drive({
+      version: 'v3',
+      auth,
+    })
+
+
+    //check for customer folder
+    const custFolder = await service.files.list({
+      q: `mimeType='application/vnd.google-apps.folder' and name='${customerId}' and parents='${folderId}'`     //this if for looking for the customer folder
+    })
+
+    console.log(custFolder.data.files);
+
+
+    let newFolder;
+    if (custFolder.data.files.length === 0){
+      newFolder = await _createFolder(customerId, folderId);
+      console.log(newFolder.data)
+    }
+
+    const partParentFolder = custFolder.data.files[0]?.id || newFolder.data.id;
+
+
+
+    //check for part folder
+    const partFolder = await service.files.list({
+      q: `mimeType='application/vnd.google-apps.folder' and name='${partNumber}-${partRev}' and parents='${partParentFolder}'`     //this if for looking for the part folder
+    })
+
+    console.log(partFolder.data.files);
+
+    let newPartFolder;
+    if (partFolder.data.files.length === 0){
+      const pfName = `${partNumber}-${partRev}`;
+      newPartFolder = await _createFolder(pfName, partParentFolder);
+      console.log(newPartFolder.data)
+    }
+
+    const fileParentFolder = partFolder.data.files[0]?.id || newPartFolder.data.id;
+
+
+
+    async function _createFolder(name, parentFolder){
+      const body = {
+            mimeType: 'application/vnd.google-apps.folder',
+            name: name,
+            parents: [parentFolder],
+          };
+      const folder = await service.files.create({
+            media,
+            requestBody: body,
+            fields: 'id',
+            supportsAllDrives: true,
+          })
+          .catch((e) => bb.emit('error', e))
+
+          return folder;
+    }
+
+    
+
+
+
+    // //check if customer folder is needed
+    // if (custFolder.data.files.length === 0){
+    //   console.log('--------------customer folder doesnt exist')
+    //   const body = {
+    //     mimeType: 'application/vnd.google-apps.folder',
+    //     name: customerId,
+    //     parents: [folderId],
+    //   };
+
+    //   //create folder
+    //   await service.files.create({
+    //     media,
+    //     requestBody: body,
+    //     fields: 'id',
+    //     supportsAllDrives: true,
+    //   })
+    //   .catch((e) => bb.emit('error', e))
+    // }
+    
+
+
+    // //check for files at drive location
+    // try {
+    //   const files = await service.files.list({
+    //     // fileId: fileId,
+    //     // fields: 'parents'
+    //   })
+    //   console.log('==============');
+    //   console.log(files);
+
+      
+    // } catch (error) {
+    //   console.log(error);
+    // }
+
+    // console.log('===========')
+    // console.log(files);
+
+    //create file on google drive
+    const requestBody = {
+      mimeType,
+      name: filename,
+      parents: [ fileParentFolder ],
+      // customerId,
+      // partNumber,
+      // partRev,
+      // partDescription       //maybe this should be left out???
+    };
+    service.files.create({
       media,
       requestBody,
       fields: 'id',
-      supportsAllDrives: true
+      supportsAllDrives: true,
+      // addProperties: {
+      //   customerId,
+      //   partNumber,
+      //   partRev,
+      // }
     })
     .then(() => bb.destroy())
     .catch((e) => bb.emit('error', e));
@@ -133,9 +359,6 @@ async function upload(req, res){
   });
 
 
+  bb.on('close', () => res.sendStatus(201));
 
-
-
-
-  res.sendStatus(200)
 };
